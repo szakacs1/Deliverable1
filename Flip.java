9bc5bb0d4633d2a6abf5be1f5de0385170e13c69
szakacs1 <72163101+szakacs1@users.noreply.github.com>
2020-10-01 18:40:50 -0400

    Add files via upload

---
+++ Flip.java
@@ -0,0 +1,11 @@
+
+public class Flip {
+	public static void main(String[] args0) {
+		if (Math.random() < 0.5) {
+			System.out.println("Heads");
+		}else {
+			System.out.println("Tails");
+		}
+	}
+
+}
