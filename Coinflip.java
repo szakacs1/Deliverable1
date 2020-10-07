4474e8741a4b814092043293cf34fe80083f78e4
szakacs1 <72163101+szakacs1@users.noreply.github.com>
2020-10-07 14:08:34 -0400

    Update Coinflip.java

--- Coinflip.java
+++ Coinflip.java
@@ -1,65 +1,67 @@
 import java.util.Scanner;
-import java.util.Random;
+
 public class Coinflip {
 	
 		
 	public static void main(String[] args0) {
 		
 		
-		System.out.println("Guess which will have more: heads or tails?");
-		Scanner scnr = new Scanner(System.in);
-		String headsOrTailsGuess = scnr.nextLine();
-		
-		Scanner scnn = new Scanner(System.in);
-	    System.out.print("How many times do you want to flip the coin? ");
-	    int numberOfFlips = scnn.nextInt();
-	    Random randomNum = new Random();
-	    
+			String headsOrTailsGuess = "";
 
-	    for (int i=0; i < numberOfFlips; i++) {
-	        int currentflip = randomNum.nextInt(2);
-	        int previousFlip = 0;
+		    int numberOfFlips = 0;
+		    int correctCount = 0;
 	        int tailsCount = 0;
 	        int headsCount = 0;
-	        int correctCount = 0;
+	        int correctCount1 = 0;
+	        int l = 0; 
+	      
+	        Scanner scnr = new Scanner(System.in);
+			System.out.println("Guess which will have more: heads or tails?");
+			headsOrTailsGuess = scnr.next();
+			
+		    System.out.print("How many times do you want to flip the coin? ");
+		    numberOfFlips = scnr.nextInt();
+		    Random randomNum = new Random();
 	        
-	        if (currentflip == 0) {
-	            System.out.print(" Heads");
-	            previousFlip = 0;
-	            headsCount++;
-	            
-	        }
-	        else if (currentflip == 1) {
-	            System.out.print(" Tails");
-	            previousFlip = 1;
-	            tailsCount++;
-	            
-	        }
+	        if (headsCount == 0) {
+				while (numberOfFlips > l) {
+					int rand = (int) Math.round(Math.random());
+					if (rand % 2 == 0.0) {
+						System.out.println("heads");
+						correctCount1++;
+						l++;
+						} else {
+						System.out.println("tails");
+						l++;
+				}
+			}
+				} else if (tailsCount == 0) {
+				while (numberOfFlips > l) {
+					int rand = (int) Math.round(Math.random());
+					if (rand % 2 == 0.0) {
+						System.out.println("heads");
+						l++;
+						} else {
+						System.out.println("tails");
+						correctCount1++;
+						l++;
+				}
+			}
+				} else {
+				String x = "Guess heads or tails only.";
+				System.out.println(x);
+			}
+			
+			System.out.println("Your guess, " + headsOrTailsGuess + ", came up " + correctCount1 + " time(s).");
+			System.out.println("That's " + correctCount1 * 100 / numberOfFlips + "%.");
+			scnr.close();
+		}
+			}
 
-	        if (previousFlip == 0 && currentflip == 1) {
-	            System.out.print(headsCount + " ");
-	            headsCount = 0;
-	        }
-	        else if (previousFlip == 1 && currentflip == 0){
-	            System.out.print(tailsCount + " ");
-	            tailsCount = 0;
-	        
-	            System.out.println("Heads = " + headsCount +  " Tails = " + tailsCount);
-	            
-	            int percent = (correctCount/numberOfFlips)*100;
-	    		System.out.println("Your percent correct is:" + percent);
-	        }
-	        {
-	        }
-	        
-	        }
-	        	
-	        	{
-	        
-	        	}
-	     
-	    }
-}
+	
+	
+	
+	
 	
 
 	    
